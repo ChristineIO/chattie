209638e2909dedad3ce10dc6113dd99c1772575f
ChristineIO <lifeanddigit@gmail.com>
2025-07-20 09:39:21 +0100

    server ready for development

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const port = process.env.PORT || 5000
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to my server!" })
+})
+
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
+});
