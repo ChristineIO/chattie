d49581255f4d3eeffc84c87493d8740cade4312f
ChristineIO <lifeanddigit@gmail.com>
2025-07-22 10:27:30 +0100

    register page updated!

--- client/src/app/components/TextArea.tsx
+++ client/src/app/components/TextArea.tsx
@@ -1,24 +1,23 @@
 import { HTMLAttributes } from "react";
 import forms from "../styles/forms.module.css"
 
-type InputFieldProps = {
+type TextAreaProps = {
     htmlFor?: string;
     name: string;
     label?: string;
-    type: string;
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
     classname?: string;
     id?: string;
     value?: string;
     maxLength?: number;
 }
 
-export default function InputField({ label, name, type, onChange, maxLength, id, value, classname } : InputFieldProps) {
+export default function TextArea({ label, name, onChange, maxLength, id, value, classname } : TextAreaProps) {
     return (
         <>
             <div className={forms.inputField}>
                 <label htmlFor={name}>{label}</label>
-                <input name={name} type={type} onChange={onChange} maxLength={maxLength} id={id} value={value} autoComplete="true" className={classname} />
+                <textarea name={name} onChange={onChange} maxLength={maxLength} id={id} value={value} autoComplete="true" className={classname} />
             </div>
         </>
     )
--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { createUser, getUser } from "@/api/api"
 import React, { useEffect, useState } from "react"
+import forms from "../styles/forms.module.css"
+import InputField from "../components/InputField"
+import TextArea from "../components/TextArea"
 
 export default function RegisterPage() {
     let [form, setForm] = useState({
@@ -11,7 +14,7 @@ export default function RegisterPage() {
         bio: "",
     })
 
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    function handleChange(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
         setForm({ ...form, [e.target.name]: e.target.value });
     }
 
@@ -21,74 +24,18 @@ export default function RegisterPage() {
         console.log(response)
     }
     return (
-        <main>
-            <form onSubmit={createNewUser}>
-                <fieldset>
-                    <legend>Register</legend>
-
-                    <section>
-                        <label htmlFor="name">Name:</label>
-                        <input
-                            type="text"
-                            id="name"
-                            name="name"
-                            value={form.name}
-                            onChange={handleChange}
-                            required
-                        />
-                    </section>
-
-                    <section>
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            name="username"
-                            value={form.username}
-                            onChange={handleChange}
-                            required
-                        />
-                    </section>
-
-                    <section>
-                        <label htmlFor="email">Email:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            value={form.email}
-                            onChange={handleChange}
-                            required
-                        />
-                    </section>
-
-                    <section>
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            value={form.password}
-                            onChange={handleChange}
-                            required
-                        />
-                    </section>
-
-                    <section>
-                        <label htmlFor="bio">Bio:</label>
-                        <input
-                            type="text"
-                            id="bio"
-                            name="bio"
-                            value={form.bio}
-                            onChange={handleChange}
-                        />
-                    </section>
-
-                    <section>
-                        <button type="submit">Submit</button>
-                    </section>
-                </fieldset>
+        <main className="flex items-center justify-center">
+            <form onSubmit={createNewUser} className={`w-3/6 ${forms.formBox}`}>
+                <h1 className="text-3xl">Welcome</h1>
+                <InputField label="Name" type="text" name="name" id="name" value={form.name} onChange={handleChange} maxLength={40} classname="w-96" />
+                <InputField label="Username" type="text" name="username" id="username" value={form.username} onChange={handleChange} maxLength={40} classname="w-96" />
+                <InputField label="Email" type="email" name="email" id="email" value={form.email} onChange={handleChange} maxLength={40} classname="w-96" />
+                <InputField label="Password" type="password" name="password" id="password" value={form.password} onChange={handleChange} maxLength={40} classname="w-96" />
+                <InputField label="Bio" type="text" name="bio" id="bio" value={form.bio} onChange={handleChange} maxLength={40} classname="w-96 h-9/10" />
+                <TextArea label="Bio" name="bio" id="bio" value={form.bio} onChange={handleChange} maxLength={40} classname="w-96 h-9/10" />
+                <section className="mt-2.5">
+                    <button type="submit" className={`pt-2.5 pb-2.5 pl-4 pr-4 rounded-xl${forms.formBtn}`}>Register</button>
+                </section>
             </form>
         </main>
     )
