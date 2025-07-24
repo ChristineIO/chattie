7ea8c91ad2ad7e75c9502315539b6f6e89d6fc67
ChristineIO <lifeanddigit@gmail.com>
2025-07-24 13:16:03 +0100

    middleware console logs deleted.

--- client/middleware.ts
+++ client/middleware.ts
@@ -2,9 +2,7 @@ import { NextResponse, NextRequest } from 'next/server'
 import { authCheck } from './api/api'
 
 export async function middleware(request: NextRequest) {
-    console.log("cnjkbfd")
     let auth = await authCheck()
-    console.log(auth)
     const url = request.nextUrl.clone()
     if (url.pathname === '/chat' && !auth.success) {
         url.pathname='/login'
