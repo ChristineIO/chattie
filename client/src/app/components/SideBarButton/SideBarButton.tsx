010196e6f86106f27ed76491c207d1a7e4a525d8
ChristineIO <lifeanddigit@gmail.com>
2025-07-26 06:45:41 +0100

    side bar button update

--- client/src/app/components/SideBarButton/SideBarButton.tsx
+++ client/src/app/components/SideBarButton/SideBarButton.tsx
@@ -3,7 +3,7 @@ import sidebarbtn from './sideBarButton.module.css'
 
 const SideBarButton = () => {
     return (
-        <button className={`text-white font-bold text-md cursor-pointer  mt-24 rounded-md w-3/5 ${sidebarbtn.button}`}>
+        <button className={`text-white font-bold text-md cursor-pointer  mt-20 rounded-md w-3/5 ${sidebarbtn.button}`}>
             <PeopleNearby className="w-8 h-8 mr-2" /> <p>     Friends </p>
         </button>
     )
