59fb0aa8be6f2ddc4a7019b2f892e9c9d0644874
ChristineIO <lifeanddigit@gmail.com>
2025-07-29 07:46:07 +0100

    ISO 8601 to regular 24 hour date format

--- client/src/app/chat/thread/[id]/page.tsx
+++ client/src/app/chat/thread/[id]/page.tsx
@@ -4,40 +4,42 @@ import { getOneThread } from "@/api/api"
 import ChatBubble from "@/src/app/components/ChatBubble/ChatBubble"
 import chatBody from "@/src/app/components/ChatBody/chatBody.module.css"
 import ChatInput from "@/src/app/components/ChatInput/ChatInput"
+import moment from "moment"
 import { use, useEffect, useState } from "react"
 
 export default function ThreadApp({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = use(params);
+    const { id } = use(params)
 
-    let [thread, setThread] = useState<any[]>([]);
+    let [thread, setThread] = useState<any[]>([])
 
 
     useEffect(() => {
         async function fetchThread() {
-            const data = await getOneThread(id);
-            const messages = data.messages;
-            setThread(messages);
+            const data = await getOneThread(id)
+            const messages = data.messages
+            setThread(messages)
 
-            console.log("Messages Array:", messages);
+            console.log("Messages Array:", messages)
         }
-        fetchThread();
+        fetchThread()
     }, [id])
 
     return (
-        <>
+        <div className={`${chatBody.bg}`}>
+            <h1 className="text-center text-5xl py-5">Your Chat Thread</h1>
             {thread.map((message: any) => (
-                <div className={`${chatBody.bg}`} key={message._id}>
+                <div key={message._id}>
                     <article className={`${chatBody.messageSection}`}>
                         <section>
-                            <ChatBubble name={message.sender} time={message.timestamp} message={message.content} />
+                            <ChatBubble name={message.sender} time={moment(message.timestamp).format('h:mm:ss A')} message={message.content} />
                         </section>
                     </article>
-                    <div>
-                        <ChatInput />
-                    </div>
                 </div>
             ))
             }
-        </>
+            <div>
+                <ChatInput />
+            </div>
+        </div>
     )
 }
